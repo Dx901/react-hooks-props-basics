d07e34638fc4f1080d0933ab1dde256e305c8f7b
Dyes Junior Sakwa <dyes.junior@student.moringaschool.com>
2022-09-20 18:41:34 +0300

    Done

--- src/components/BlogPost.js
+++ src/components/BlogPost.js
@@ -2,10 +2,25 @@ import React from "react";
 import BlogContent from "./BlogContent";
 import Comment from "./Comment";
 
-function BlogPost() {
+/*function BlogPost() {
   return (
     <div id="blog-post">
       <BlogContent articleText="Dear Reader: Bjarne Stroustrup has the perfect lecture oration." />
+      <Comment commentText="I agree with this statement. - Angela Merkel" />
+      <Comment commentText="A universal truth. - Noam Chomsky" />
+      <Comment commentText="Truth is singular. Its ‘versions’ are mistruths. - Sonmi-451" />
+    </div>
+  );
+}
+*/
+
+function BlogPost() {
+  return (
+    <div>
+      <BlogContent articleText="Dear Reader: Bjarne Stroustrup has the perfect lecture oration." />
+      isPublished={true}
+      minutesToRead={1}
+
       <Comment commentText="I agree with this statement. - Angela Merkel" />
       <Comment commentText="A universal truth. - Noam Chomsky" />
       <Comment commentText="Truth is singular. Its ‘versions’ are mistruths. - Sonmi-451" />
